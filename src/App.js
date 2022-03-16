d4a649c9c51ebafcc610e00b4b01b43d0947af56
Souaybou <souaybou.coulibaly@etu.univ-paris1.fr>
2022-03-16 16:20:19 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Activity from './activity';
+import './App.css'
 function App() {
+
+  const [activities, setActivity] = useState([]);
+  const [search, setSearch] = useState('');
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if(count <= 10){
+    axios.get('http://www.boredapi.com/api/activity/').then(response => {
+      console.log(response.data);  
+    setActivity([...activities,response.data]);
+    }).catch(error => console.log(error))
+    setCount(count +1);
+  }
+  }, [activities]);
+
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredActivities = activities.filter(activity => activity.activity.toLowerCase().includes(search.toLowerCase()))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Activity">
+      <div className="Activity-search" >
+        <h1 className="Activity-text"> Search an activity</h1>
+        <form>
+          <input type="text" placeholder="Search" className="activity-input" onChange={handleChange}/>
+        </form>
+      </div>
+       {filteredActivities.map(activity => {
+       return(
+         <Activity key={activity.key} 
+         name = {activity.activity} 
+         participants = {activity.participants}
+         type = {activity.type}
+         price = {activity.price}
+         link = {activity.link}
+       />)
+     })}
     </div>
   );
 }
